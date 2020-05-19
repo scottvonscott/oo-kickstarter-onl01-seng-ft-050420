a814026bb843fa984b1f5d820acdd6c5d397d551
Scott  Keplinger <skeplin17@gmail.com>
2020-05-19 23:56:44 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,25 @@
+class Backer
+
+  attr_accessor :backed_projects
+  attr_reader :name
+
+
+  def initialize (name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    unless project.backers.include?(self)
+      project.add_backer(self)
+    end
+  end
+
+
+
+
+
+
+
+end
